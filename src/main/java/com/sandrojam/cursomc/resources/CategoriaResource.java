2371970c55e8f480285702256789ac7b95f9bd49
SandroJAM <sandrojam@hotmail.com>
2022-10-19 14:37:05 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/sandrojam/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sandrojam/cursomc/resources/CategoriaResource.java
@@ -16,8 +16,8 @@ public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1 = new Categoria (1, "Informática");
-		Categoria cat2 = new Categoria (2, "Escritório");
+		Categoria cat1 = new Categoria (1, "Informatica");
+		Categoria cat2 = new Categoria (2, "Escritorio");
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
